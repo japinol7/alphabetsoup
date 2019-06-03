85a1a6716c854d40e8eba1e2a5cb1d4755b44773
japinol <joan@192.168.1.37>
2019-06-03 19:31:22 +0200

    Small changes.

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
     description='Alphabet Soup Solver.',
     long_description='Alphabet Soup Solver v. 1.0.2. Solves an alphabet soup finding a given list of words inside it.',
     url='https://github.com/japinol7/alphabetsoup',
-    packages = ['alphabetsoup'],
+    packages=['alphabetsoup'],
     python_requires='>=3.6',
     entry_points={
         'console_scripts': [
             'alphabetsoup=alphabetsoup.__main__:main',
-        ],
+            ],
     },
 )
